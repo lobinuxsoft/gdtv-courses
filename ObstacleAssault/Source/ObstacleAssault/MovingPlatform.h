da9ddc8603827fdfa62115095bd9b24360ca3065
Matias Galarza <lobinuxsoft@gmail.com>
2024-02-09 20:20:59 -0300

    Implement basic platform

---
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	FVector MoveDirection{ 0.f,0.f,1.f };
+
+	UPROPERTY(EditAnywhere)
+	float MoveSpeed{ 100 };
+};
