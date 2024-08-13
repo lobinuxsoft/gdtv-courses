8e4e5f0ac8489b48aca991497044023d9a2c5f03
Lobby <lobinuxsoft@gmail.com>
2024-08-12 21:29:49 -0300

    Complete section 11 and 12 of Toon Tanks project

---
+++ ToonTanks/Source/ToonTanks/Tank.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ATank::ATank()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Component"));
+	CameraComp->SetupAttachment(SpringArmComp);
+}
