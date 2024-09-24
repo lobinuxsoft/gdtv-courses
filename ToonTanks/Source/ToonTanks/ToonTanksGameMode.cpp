5abc9102cfe08a7e96b6b72766cce01639bcd7b9
Lobby <lobinuxsoft@gmail.com>
2024-09-23 22:57:00 -0300

    Complete section 5.36 ~ 37

--- ToonTanks/Source/ToonTanks/ToonTanksGameMode.cpp
+++ ToonTanks/Source/ToonTanks/ToonTanksGameMode.cpp
@@ -16,10 +16,15 @@ void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 		
 		if(ToonTanksPlayerController)
 			ToonTanksPlayerController->SetPlayerEnabledState(false);
+
+		GameOver(false);
 	}
 	else if (ATower* DestroyedTower = Cast<ATower>(DeadActor))
 	{
 		DestroyedTower->HandleDestruction();
+		TargetTowers--;
+		if(TargetTowers == 0)
+			GameOver(true);
 	}
 }
 
@@ -32,6 +37,7 @@ void AToonTanksGameMode::BeginPlay()
 
 void AToonTanksGameMode::HandleGameStart()
 {
+	TargetTowers = GetTargetTowerCount();
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
 	ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
 
@@ -44,4 +50,11 @@ void AToonTanksGameMode::HandleGameStart()
 		FTimerDelegate PlayerEnableTimerDelegate = FTimerDelegate::CreateUObject(ToonTanksPlayerController, &AToonTanksPlayerController::SetPlayerEnabledState, true);
 		GetWorldTimerManager().SetTimer(PlayerEnableTimerHandle, PlayerEnableTimerDelegate, StartDelay, false);
 	}
+}
+
+int32 AToonTanksGameMode::GetTargetTowerCount()
+{
+	TArray<AActor*> Towers;
+	UGameplayStatics::GetAllActorsOfClass(this, ATower::StaticClass(), Towers);
+	return Towers.Num();
 }
